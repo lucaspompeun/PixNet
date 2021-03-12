8832189c6809b28d689db25f1ccd6bfbd352fe59
Lucas Pompeu <lucaspompeuneves@gmail.com>
2021-03-11 21:56:59 -0300

    Primeira publicação 0.1.0 no nuget

--- PixNET/obj/Debug/netstandard2.0/PixNet.AssemblyInfo.cs
+++ PixNET/obj/Debug/netstandard2.0/PixNet.AssemblyInfo.cs
@@ -11,13 +11,13 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("PixNet")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Egua Tech")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("0.1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.1.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PixNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PixNet")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("0.1.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
 
